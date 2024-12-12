7cd6c62f9f06efdb787af0d0df4386e2792b6795
Ark21bit <webleif@vk.com>
2024-12-12 10:41:40 +0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-    <div>
-       
-    </div>
+    <span class="bg-[#db4727] rounded-[24px] text-white text-[11px] font-[700] leading-[80%] min-w-[22px] py-[3px] px-[4px] w-fit h-fit">
+       99
+    </span>
 </template>
-
 <script setup>
 
 </script>
